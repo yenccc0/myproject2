31c907da11b526798874e28eaa6b2d713ffd625c
mark455174@gmail.com <mark123456>
2022-05-13 11:32:57 +0800

    5/13

---
+++ src/main/java/com/Mark/Parking/Runner.java
@@ -0,0 +1,14 @@
+package com.Mark.Parking;
+
+import java.util.Date;
+public class Runner {
+    public static void main(String[] args) {
+        String id = "ABC-1234";
+        String entertime = "8:00";
+        String exitTime = "11:00";
+        int enter=7800;
+        int exit=7900;
+        Date d = new Date();
+        System.out.println(d);
+    }
+}
