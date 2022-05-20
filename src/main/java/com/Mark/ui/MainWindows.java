620a4c7822072f2e5157934d2542f404c67e78a5
mark455174@gmail.com <mark123456>
2022-05-20 12:57:26 +0800

    5/20 Interface;Actionlistener

--- src/main/java/com/Mark/ui/MainWindows.java
+++ src/main/java/com/Mark/ui/MainWindows.java
@@ -1,6 +1,9 @@
 package com.Mark.ui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainWindows {
     public static void main(String[] args) {
@@ -8,9 +11,27 @@ public class MainWindows {
         //swing, javaFX
         //eXtension
         JFrame frame = new JFrame();
-        frame.setVisible(true);
         frame.setSize(400,300);
         frame.setLocation(200,200);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        JButton button = new JButton("OK");
+        // Anonymous class
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("HaHaHa!!!");
+            }
+        });
+//        MyActionListener listener = new MyActionListener();
+//        button.addActionListener(listener);
+        frame.setLayout(new FlowLayout());
+        //frame.setLayout(new BorderLayout());
+        frame.add(button);
+
+
+        //Layout
+        frame.setVisible(true);
         System.out.println("End?");
+
     }
 }
